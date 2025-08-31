b124f2ec23d08135988bd01c79378cd4349bd9ff
ASUS <nethmidileksha6@gmail.com>
2025-08-31 18:51:09 +0530

    create testing router part

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import "./../global.css"
+import { useRouter } from "expo-router";
 
 export default function Index() {
+
+  const router = useRouter();
+  
   return (
     <View className="flex-1 items-center justify-center bg-red-600">
       <Text className="text-white text-lg font-bold">
         Edit app/index.tsx to edit this screen.
       </Text>
+      <TouchableOpacity onPress={() => router.push("/testing")}>
+        <Text>
+          change router
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
